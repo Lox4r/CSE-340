e651313a6841c8231c7f16b0363552c7b0fe62ae
Lox4r <loxhand@gmail.com>
2026-03-15 01:38:25 -0500

    1

---
+++ datebase/rebuild.sql
@@ -0,0 +1,18 @@
+CREATE TYPE
+CREATE TABLE classification
+CREATE TABLE inventory
+CREATE TABLE account
+INSERT classification data
+INSERT inventory data
+
+-- Query 4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
+
+-- Query 6
+UPDATE inventory
+SET
+inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
